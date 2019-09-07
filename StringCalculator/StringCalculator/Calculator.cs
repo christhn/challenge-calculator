5c9fca8f70be212d38438a3f4377c4e11e7dd1c6
Chris Nguyen <chrisnguyen@gmail.com>
2019-09-06 21:53:03 -0700

    Support multiple delimiters of any length

--- StringCalculator/StringCalculator/Calculator.cs
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -8,7 +8,7 @@ namespace StringCalculator
     {
         private static Calculator calc = new Calculator();
 
-        private void Calculate(String input)
+        private void Calculate(string input)
         {
             input = calc.NormalizeString(input);
             string[] numbers = input.Split(',');
@@ -39,35 +39,46 @@ namespace StringCalculator
         }
 
         // I'm certain there's a better way to do this (possibly with regex) but would need more time
-        private String NormalizeString(String input)
+        // Just thinking outloud: can this also be done with a configuration file or a map?
+        private string NormalizeString(String input)
         {
             // remove the // and create a common delimiter to normalize the string
             int index = input.IndexOf("//");
-            String delimiter = null;
+            string delimiter = null;
             if (index == 0)
             {
                 if (input.Length > 2)
                 {
                     input = input.Remove(0, 2);
-                    if (input[0].Equals('['))
+                    if (!input[0].Equals('['))
                     {
-                        delimiter = input.Substring(1, input.IndexOf(']') - 1);
-                        input = input.Remove(0, input.IndexOf(']') - 1);
+                        delimiter = input[0].ToString();
+                        input = calc.swapDelimiter(input, delimiter);
                     }
                     else
                     {
-                        delimiter = input[0].ToString();
-                    }
-                    if (input.Contains(delimiter))
-                    {
-                        input = input.Replace(delimiter, ",");
+                        while (input[0].Equals('['))
+                        {
+                            delimiter = input.Substring(1, input.IndexOf(']') - 1);
+                            input = input.Remove(0, input.IndexOf(']') + 1);
+                            input = calc.swapDelimiter(input, delimiter);
+                        }
                     }
                 }
             }
 
-            if (input.Contains("\\n"))
+            delimiter = ("\\n");
+            input = calc.swapDelimiter(input, delimiter);
+
+            return input;
+        }
+
+        // convert all delimiters to commas for easier management
+        private string swapDelimiter(String input, String delimiter)
+        {
+            if (input.Contains(delimiter))
             {
-                input = input.Replace("\\n", ",");
+                input = input.Replace(delimiter, ",");
             }
 
             return input;
@@ -88,7 +99,7 @@ namespace StringCalculator
         static void Main(string[] args)
         {
             Console.Write("Input string: ");
-            String input = Console.ReadLine();
+            string input = Console.ReadLine();
             calc.Calculate(input);
             Console.ReadLine();
         }
